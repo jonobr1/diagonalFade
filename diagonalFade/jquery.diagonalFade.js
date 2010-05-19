0266b32d55e6dbb91df5f99881af2b60158da798
jonobr1 <jonobrandel@gmail.com>
2010-05-19 14:29:11 -0700

    Initial commit of jquery diagonalFade Plugin

---
+++ diagonalFade/jquery.diagonalFade.js
@@ -0,0 +1,10 @@
+$.fn.diagonalFade = function() {
+    var options = $.extend({},$.fn.count.defaultOptions, customOptions);
+    return this.each.(function() {
+        var $this = $(this);
+    });
+};
+
+$.fn.diagonalFade.defaultOptions = {
+    startCount: 100
+};
