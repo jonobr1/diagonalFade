f7c7609653cd4413be25e86474659a5c73fe520c
Jono Brandel <jonobrandel@google.com>
2011-04-14 15:40:05 -0700

    updated with Apache License 2.0

--- j/jquery.diagonalFade.js
+++ j/jquery.diagonalFade.js
@@ -1,6 +1,6 @@
-/*!
+/**
  * jQuery Diagonal Matrix Fade Plugin
- * Examples and documentation at: http://jonobr1.com/
+ * Examples and documentation at: http://jonobr1.github.com/diagonalFade
  * Copyright (c) 2010 Jono Brandel
  * Version: .01 (18-MAY-2010)
  * Requires: jQuery v1.3.2 or later
@@ -8,6 +8,20 @@
  * To Do:
  * + Extend Cycle or write own (based on jquery.cycle.lite.js)
  * + Make it so it's not just for a fixed container
+ *
+ * Copyright 2010 jonobr1
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
  
 (function($) {
